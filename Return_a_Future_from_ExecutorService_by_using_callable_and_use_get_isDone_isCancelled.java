ea21f4b266b8e7eec1edfd6fc8809ff8ace80ac7
japneetkaur21 <47344108+japneetkaur21@users.noreply.github.com>
2019-02-28 18:31:44 +0530

    Add files via upload

---
+++ Return_a_Future_from_ExecutorService_by_using_callable_and_use_get_isDone_isCancelled.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.*;
+
+public class Return_a_Future_from_ExecutorService_by_using_callable_and_use_get_isDone_isCancelled {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> integerFuture = executorService.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return 2;
+            }
+        });
+
+        executorService.shutdown();
+
+        if (integerFuture.isDone()) {
+            System.out.println("The integer is : "+integerFuture.get());
+        }
+
+        if(integerFuture.isCancelled()){
+            System.out.println("Your task has been cancelled");
+        }
+    }
+}
